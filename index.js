7dc0f30e3b3204585075846fe99aad97d64d1814
Azrail <azrailcecoev326@gmail.com>
2023-05-27 15:51:23 +0300

    third commit

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 // const morgan = require("morgan");
 const express  = require("express")
 const multer  = require("multer");
+const upload = multer({dest: "files"})
 const productsRouter = require('./routes/products.routes')
 const app = express(); 
 const PORT = 3000;
 
-  
-const storageConfig = multer.diskStorage({
-    destination: (req, file, cb) =>{
-        cb(null, "uploads");
-    },
-    filename: (req, file, cb) =>{
-        cb(null, file.originalname);
+const storageConfig = multer.diskStorage(
+    {
+       destination: (req, file, cb) =>{
+        cb(null, "files")
+       },
+       filename: (req, file, cb) => {
+        cb(null, file.originalname)
     }
-});
+    }
+)
 
- 
-app.use(multer({storage:storageConfig}).single("filedata"));
-app.post("/upload", function (req, res, next) {
-   
+app.use(express.static(__dirname));
+app.use(multer({storage: storageConfig}).single("filedata"))
+app.get("/upload", function (req, res, next) {
     let filedata = req.file;
     if(!filedata)
         res.send("Ошибка при загрузке файла");
